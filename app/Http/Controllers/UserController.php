7bdf1525f4505febc304b1d6169904efc38078cd
magdev09 <magdev09@gmail.com>
2023-03-19 00:55:13 +0300

    прописал метод добавления юзера в контроллер,добавил форму во вьюху юзера

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -34,7 +35,13 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = new User();
+        $user->name = $request->name;
+        $user->lastname = $request->lastname;
+        $user->fathername = $request->fathername;
+        $user->phone = $request->phone;
+
+        $user->save();
     }
 
     /**
